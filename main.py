1e7fc51676a3bcfb1e4e2cc2f06db10737761fab
Кирилл Грибанов <gribanov.k@mamod.ai>
2025-01-13 09:26:50 +0300

    elimt hst

--- main.py
+++ main.py
@@ -81,8 +81,8 @@ async def start_client(session_name):
         except Exception as e:
             logger.error(e)
 
-    #await client.connect()
-    await client.run_until_disconnected()
+    await client.connect()
+    #await client.run_until_disconnected()
 
     running_clients.append((session_name, client))
 
