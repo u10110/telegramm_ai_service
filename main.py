f9ff41221fab4caea599448f4e27e6d2076647db
Кирилл Грибанов <gribanov.k@mamod.ai>
2025-01-13 14:38:30 +0300

    fix onmessage di fr to chat

--- main.py
+++ main.py
@@ -84,8 +84,8 @@ async def start_client(session_name):
             except Exception as e:
                 logger.error(e)
 
-    client.start()
-    client.run_until_disconnected()
+    await client.connect()
+    #await client.run_until_disconnected()
 
     running_clients.append((session_name, client))
 
