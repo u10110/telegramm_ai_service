babe110ea4c2c483c94cf885705b2626f2215921
Кирилл Грибанов <gribanov.k@mamod.ai>
2025-01-17 21:02:43 +0300

    ch producer init

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ phone_hash_store = {}
 
 running_clients = {}
 KAFKA_BOOTSTRAP_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS")
-producer = KafkaProducer(bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,  api_version=(0,11,5))
+producer = KafkaProducer(bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,  api_version=(3,9,0))
 
 
 APP_HOST = os.getenv("APP_HOST")
