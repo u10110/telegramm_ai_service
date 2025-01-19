eb8800c7bdcd2e7be4d3f2281c5afb472cbd6267
Кирилл Грибанов <gribanov.k@mamod.ai>
2025-01-20 02:40:15 +0300

    del start

--- main.py
+++ main.py
@@ -67,9 +67,7 @@ async def startup_event():
             session_name = filename.split('.')[0]
             phone = session_name.split('_')[1]
             client = await get_create_client(phone)
-            await client.start()
-            await client.run_until_disconnected()
-
+            await client.connect()
 
 
 async def get_create_client(phone):
